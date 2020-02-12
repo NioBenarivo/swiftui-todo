0fe7008e2e807c13faff220d4817620027c4973d
Zunio Benarivo <zunibenarivo@gmail.com>
2020-02-12 09:32:09 +0700

    Initial commit

---
+++ SwiftUIToDo/SwiftUIToDo/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIToDo
+//
+//  Created by Nakama on 06/02/20.
+//  Copyright © 2020 Nakama. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsViewController: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello World")
+        }
+    }
+}
+
+struct TodoViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsViewController()
+    }
+}
